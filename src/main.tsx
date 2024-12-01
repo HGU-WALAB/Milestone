5f7b95457227210aa7eaae209a4e6dcf68c4de51
Yoomin Hwang <yoominh3@gmail.com>
2024-12-01 14:28:58 +0900

    Chore: HeaderBar 주변 html, body tag 의 기본 margin 값 없앰 css 폴더 만듦 .github 파일에 pull_request_template.md 올림

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { Routing } from './routes/Routing'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RecoilRoot } from "recoil";
+import { BrowserRouter } from 'react-router-dom';
+import './css/index.css';
+import App from './App';
 
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Routing />
-  </StrictMode>,
+  <RecoilRoot> {/* RecoilRoot로 전체 앱을 감싸서 상태 관리 기능 활성화 */}
+    <BrowserRouter>
+      <StrictMode>
+        <App/>
+      </StrictMode>
+    </BrowserRouter>
+  </RecoilRoot>
 )
--- src/routes/Routing.tsx
+++ src/routes/Routing.tsx
@@ -1,19 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import HeaderBar from '../components/HeaderBar';
 import ResumePage from '../pages/ResumePage';
 import ActivitiesPage from '../pages/ActivitiesPage';
 import ProfilePage from '../pages/ProfilePage';
-import App from '../App';
-import { Outlet } from 'react-router-dom';
+import MainPage from '../pages/MainPage';
 
 function Layout () {
   return (
-    <>
+    <div>
       <HeaderBar />
       <main>
         <Outlet /> {/* 하위 라우트를 렌더링 */}
       </main>
-    </>
+    </div>
   );
 };
 
@@ -21,16 +20,14 @@ export default Layout;
 
 export const Routing = () => {
   return (
-    <Router>
       <Routes>
         {/* Layout이 HeaderBar를 포함하므로 HeaderBar를 중첩 라우터에 포함하지 X */}
         <Route path="/" element={<Layout />}>
-          <Route index element={<App />} />
+          <Route index element={<MainPage />} />
           <Route path="resume" element={<ResumePage />} />
           <Route path="activities" element={<ActivitiesPage />} />
           <Route path="profile" element={<ProfilePage />} />
         </Route>
       </Routes>
-    </Router>
   );
 };
